2f2b1deef38d375275fec2e27518cb2f6cd3d9dd
SSDmitiry <XYZmail@yandex.ru>
2022-11-12 11:42:18 +0500

    Add project files.

---
+++ DASCII_Snake/Program.cs
@@ -0,0 +1,22 @@
+﻿using static System.Console;
+
+
+namespace DASCII_Snake
+{
+    class Program
+    {
+        //global vars
+        private const int MAP_WIDTH = 30;
+        private const int MAP_HEIGHT = 20;
+
+        static void Main(string[] args)
+        {
+            SetWindowSize(MAP_WIDTH, MAP_HEIGHT);
+            SetBufferSize(MAP_WIDTH, MAP_HEIGHT);
+            CursorVisible = false;
+
+            ReadKey();
+
+        }
+    }
+}
